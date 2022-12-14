55b8c454fa349d44ef04907edb404bcafa2bfbc6
Vo Quang Chien <chiendien2006@gmail.com>
2022-12-14 15:52:12 +0700

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
 starship init fish | source
-set fish_greeting ""
+set -U fish_greeting
 set -gx TERM xterm-256color
+
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx /usr/bin/i3 -- -keeptty
+    end
+end
