aecf68c9eb7087c34943460f24c05fa1fba1c059
Vo Quang Chien <chiendien2006@gmail.com>
2022-12-22 22:13:04 +0700

    add dunst

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 # Flameshot
+if [[ `pidof flameshot` ]]; then
+    pkill flameshot
+fi
+
 flameshot
+
+# Dunst
+if [[ `pidof dunst` ]]; then
+	pkill dunst
+fi
+
+dunst -config ~/.config/dunst/dunstrc &
