a992bd59fdb8f5f97ab0d6c3bca081598d9a2d31
yeuxacucodon <chiendien2006@gmail.com>
2023-01-06 15:29:24 +0700

    update

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -9,19 +9,18 @@ flameshot &
 if [[ `pidof dunst` ]]; then
 	pkill dunst
 fi
-
-dunst -config ~/.config/dunst/dunstrc &
+dunst -config ~/.config/dunst/dunstrc
 
 # Picom
 killall -q picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 picom --config ~/.config/picom/picom.conf &
 
-# Polybar
-bash ~/.config/polybar/launch.sh &
-
 # Fix cursor
 xsetroot -cursor_name left_ptr
 
 # Set background
 feh --bg-scale ~/.local/share/backgrounds/yo.jpg
+
+# Polybar
+~/.config/polybar/launch.sh
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,4 +11,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+polybar -q main -c "$DIR"/config.ini
