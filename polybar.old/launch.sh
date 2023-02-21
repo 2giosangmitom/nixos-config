f41ff1ec018f67369526688a7ac85f68d7aca245
Vo Quang Chien <chiendien2006@gmail.com>
2023-02-21 21:57:05 +0700

    fix sep color

--- polybar.old/launch.sh
+++ polybar.old/launch.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
 DIR="$HOME/.config/polybar"
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -q main -c "$DIR"/config.ini &
+polybar -q main -c "$DIR"/config.ini
