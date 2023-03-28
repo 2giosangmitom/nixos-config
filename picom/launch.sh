5fae8546fc370f4a533762220192b9d47733c0a1
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-03-28 21:21:41 +0700

    fix screen tearing

--- picom/launch.sh
+++ picom/launch.sh
@@ -2,9 +2,6 @@
 
 DIR="$HOME/.config/picom"
 
-if [[ $(pgrep -x picom) ]]; then
-	killall -q picom
-	while pgrep -x picom >/dev/null; do sleep 1; done
-fi
-
+killall -q picom
+while pgrep -x picom >/dev/null; do sleep 1; done
 picom --config "$DIR"/picom.conf &
