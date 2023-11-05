0a117c1134a6716a9555fbdd081d48c1e03a7adb
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-05 19:03:42 +0700

    Lien Chi

--- sway/dynamic_wallpaper.sh
+++ sway/dynamic_wallpaper.sh
@@ -2,4 +2,4 @@
 
 killall -q swaybg
 while pgrep -x swaybg >/dev/null; do sleep 1; done
-swaybg -i $(find ~/.config/sway/wallpaper/* | shuf -n1) -m fill &
+swaybg -i $(find ~/.config/sway/wallpaper/* | shuf -n1) -m fill
