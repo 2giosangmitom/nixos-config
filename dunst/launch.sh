7f328df9b6eb84a89a2b537482cff8ed5a9feb2a
Vo Quang Chien <voquangchien.dev@proton.me>
2023-12-30 13:30:02 +0700

    tokyonight colors

--- dunst/launch.sh
+++ dunst/launch.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+# Terminate if dunst is already running
 killall -q dunst
+
+# Wait until the processes have been shut down
 while pgrep -x dunst >/dev/null; do sleep 1; done
+
+# Launch dunst
 dunst &
