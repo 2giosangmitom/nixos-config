ed565f274da031af746bbfeb51b1ea9a094fd505
Vo Quang Chien <voquangchien.dev@proton.me>
2023-12-30 13:33:04 +0700

    add scripts

---
+++ scripts/gtk_config_wayland.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+gsettings set org.gnome.desktop.interface cursor-theme Sweet-cursors
+gsettings set org.gnome.desktop.interface icon-theme Tela-dark
+gsettings set org.gnome.desktop.interface gtk-theme Sweet-Arbar-Blue-Dark-v40
+gsettings set org.gnome.desktop.interface font-name 'Roboto 10'
+gsettings set org.gnome.desktop.interface enable-animations true
