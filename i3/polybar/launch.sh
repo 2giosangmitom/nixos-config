99dd2b4b6193858c8f3d36dbcd001fc321115772
Vo Quang Chien <voquangchien.dev@proton.me>
2023-12-30 15:01:30 +0700

    i3: fix polybar launch script

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -4,7 +4,7 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 polybar &
