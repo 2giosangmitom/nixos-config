cf1a08bf51d2364d61e1bcbbab136579857de3ad
Vo Quang Chien <voquangchien.dev@proton.me>
2023-12-30 19:11:00 +0700

    i3: fix picom launch script

--- i3/picom/launch.sh
+++ i3/picom/launch.sh
@@ -4,7 +4,7 @@
 killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+while pgrep -x picom >/dev/null; do sleep 1; done
 
 # Launch picom
 picom --config "$HOME/.config/picom/picom.conf" &
