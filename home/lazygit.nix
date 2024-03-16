f8ff1524f30e1af1d8fa7388cdc4b2e9b9172a9b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-16 07:35:03 +0700

    feat: add lazygit config

---
+++ home/lazygit.nix
@@ -0,0 +1,15 @@
+{
+  enable = true;
+  settings = {
+    gui = {
+      windowSize = "normal";
+      sidePanelWidth = 0.25;
+      language = "en";
+      showRandomTip = false;
+      showBottomLine = false;
+    };
+    os = {
+      editPreset = "nvim";
+    };
+  };
+}
