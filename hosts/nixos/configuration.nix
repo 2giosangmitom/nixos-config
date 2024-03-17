9b4ce2d726dc04866223791acbe5f3c37a8065da
Vo Quang Chien <voquangchien.dev@proton.me>
2024-03-17 20:27:38 +0700

    refactor: make code more reusable

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -5,7 +5,6 @@
 }: {
   imports = [./hardware-configuration.nix];
 
-  networking.hostName = "nixos";
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
   environment.systemPackages = with pkgs; [neovim git];
