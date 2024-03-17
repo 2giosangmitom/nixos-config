6c57cd9a186e7f6c14734c5dc1c62af6e0b762cc
Vo Quang Chien <voquangchien.dev@proton.me>
2024-03-17 09:32:04 +0700

    fix(hyprland): correct mouse binding config

--- modules/programs.nix
+++ modules/programs.nix
@@ -8,15 +8,23 @@
     enable = true;
     enableSSHSupport = true;
   };
+
   programs.dconf.enable = true;
-  programs.hyprland.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
   programs.xwayland.enable = true;
+
   xdg.portal = {
     enable = true;
     wlr.enable = true;
     xdgOpenUsePortal = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
     ];
   };
 }
