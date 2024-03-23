618a44cb6b3a07f90a16fbc71b19e30023dfdb85
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-23 13:58:07 +0700

    refactor(input_method): improve script formatting and functionality
    Refactored the bash script to enhance readability and maintainability. Changes include consistent quoting of variables, simplification of notification messages, and the use of `printf` over `echo` for better portability. The script now adheres to best practices and provides a clearer user experience.

--- home/scripts/input_method.sh
+++ home/scripts/input_method.sh
@@ -3,16 +3,16 @@
 input_method=$(ibus engine)
 
 display_help() {
-	echo -e "\033[1;97mUsage:\033[0m"
-	echo -e "\t$0 [option]"
-	echo -e "\033[1;97mOptions:\033[0m"
-	echo -e "\t-c, --check\tCheck for current input method"
-	echo -e "\t-t, --toggle\tToggle input method"
+	printf "\033[1;97mUsage:\033[0m\n"
+	printf "\t%s [option]\n" "$0"
+	printf "\033[1;97mOptions:\033[0m\n"
+	printf "\t-c, --check\tCheck for current input method\n"
+	printf "\t-t, --toggle\tToggle input method\n"
 }
 
 check_input_method() {
-	if [[ $input_method == "" ]]; then
-		notify-send "No engine is set"
+	if [[ -z "$input_method" ]]; then
+		notify-send "No input method is set"
 	else
 		notify-send "Input method: $input_method"
 	fi
@@ -20,20 +20,23 @@ check_input_method() {
 
 toggle_input_method() {
 	case $input_method in
-	"Bamboo")
+	Bamboo)
 		ibus engine "BambooUs"
 		notify-send "Switched to English"
 		;;
-	"BambooUs" | "xkb:us::eng")
+	BambooUs | xkb:us::eng)
 		ibus engine "Bamboo"
 		notify-send "Switched to Vietnamese"
 		;;
-	*) notify-send "No engine is set" ;;
+	*)
+		notify-send "No input method is set"
+		;;
 	esac
 }
 
 case $1 in
-"-h" | "--help") display_help ;;
-"-c" | "--check") check_input_method ;;
-"-t" | "--toggle") toggle_input_method ;;
+-h | --help) display_help ;;
+-c | --check) check_input_method ;;
+-t | --toggle) toggle_input_method ;;
+*) display_help ;;
 esac
