20888efaee6cec179d4929faee82cad58b2c16af
Vo Quang Chien <voquangchien.dev@proton.me>
2024-03-23 13:17:02 +0700

    feat(hyprland): add `swallow_regex`

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -70,6 +70,7 @@
         disable_splash_rendering = true;
         mouse_move_enables_dpms = true;
         enable_swallow = true;
+        swallow_regex = "^(Alacritty)$";
         key_press_enables_dpms = true;
         vfr = true;
       };
