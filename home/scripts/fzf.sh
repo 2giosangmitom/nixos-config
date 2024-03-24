afd6a785c9c3a2a136a09a104607744f15371655
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-24 13:52:38 +0700

    feat(fzf): checkout recent branch

--- home/scripts/fzf.sh
+++ home/scripts/fzf.sh
@@ -4,6 +4,11 @@ e() {
 	nvim $(fzf --preview 'bat --style=numbers --color=always {}' --border -m)
 }
 
+checkout_recent_branch() {
+	git branch --sort=-committerdate | fzf --preview "git diff {1}" --border | xargs git checkout
+}
+
 case $1 in
 e) e ;;
+checkout_recent_branch) checkout_recent_branch ;;
 esac
