64acbfacc7572b88307797398295bcd11c8c5590
Vo Quang Chien <voquangchien.dev@proton.me>
2024-03-24 20:30:57 +0700

    feat(waybar): add temperature module

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -1,6 +1,7 @@
-{
+{pkgs-unstable, ...}: {
   programs.waybar = {
     enable = true;
+    package = pkgs-unstable.waybar;
     settings = {
       mainBar = {
         layer = "top";
@@ -12,7 +13,7 @@
         height = 0;
         modules-left = ["clock" "hyprland/workspaces"];
         modules-center = ["custom/uptime"];
-        modules-right = ["pulseaudio" "cpu" "memory" "tray"];
+        modules-right = ["pulseaudio" "temperature" "cpu" "memory" "tray"];
         clock = {
           format = "{:%b %d - %H:%M}";
           tooltip = false;
@@ -26,21 +27,55 @@
           format = "{}";
         };
         pulseaudio = {
-          format = "<span color='#b4f9f8'>{icon}</span>{volume}%";
+          format = "<span color='#cba6f7'>{icon}</span>{volume}%";
           tooltip = false;
-          format-muted = "<span color='#f7768e'> Muted</span>";
+          format-muted = "<span color='#f38ba8'> </span>Muted";
           format-icons = {
             default = [" " " " " "];
           };
           on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
           scroll-step = 5;
         };
+        temperature = {
+          tooltip = false;
+          format = "<span color='#ea76cb'>{icon}</span> {temperatureC}°C";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
         cpu = {
-          format = "<span color='#bb9af7'></span> {usage}%";
+          format = "<span color='#eba0ac'>{icon}</span>{usage}%";
+          format-icons = [
+            " "
+            "󰪞 "
+            "󰪟 "
+            "󰪠 "
+            "󰪡 "
+            "󰪢 "
+            "󰪣 "
+            "󰪤 "
+            "󰪥 "
+          ];
           tooltip = false;
         };
         memory = {
-          format = "<span color='#ff9e64'>󰍛</span> {used}/{total}";
+          format = "<span color='#fab387'>{icon}</span>{used}/{total}";
+          format-icons = [
+            " "
+            "󰪞 "
+            "󰪟 "
+            "󰪠 "
+            "󰪡 "
+            "󰪢 "
+            "󰪣 "
+            "󰪤 "
+            "󰪥 "
+          ];
           tooltip = false;
         };
       };
