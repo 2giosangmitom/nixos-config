18e3bed8bedb500202f1f867c55adb282a31ff5a
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-27 20:13:50 +0700

    feat(hyprland): move waybar config to hyprland

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -1,5 +1,11 @@
 {pkgs-unstable, ...}: {
   systemd.user.targets.hyprland-session.Unit.Wants = ["xdg-desktop-autostart.target"];
+
+  home.packages = with pkgs-unstable; [
+    hyprpaper
+    grimblast
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
     package = pkgs-unstable.hyprland;
@@ -134,5 +140,140 @@
         "$mod,mouse:273,resizewindow"
       ];
     };
+  };
+
+  programs.waybar = {
+    enable = true;
+    package = pkgs-unstable.waybar;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        mod = "dock";
+        exclusive = true;
+        passthrough = false;
+        gtk-layer-shell = true;
+        height = 0;
+        modules-left = ["clock" "hyprland/workspaces"];
+        modules-center = ["custom/uptime"];
+        modules-right = ["pulseaudio" "temperature" "cpu" "memory" "tray"];
+        clock = {
+          format = "{:%b %d - %H:%M}";
+          tooltip = false;
+        };
+        "hyprland/workspaces" = {
+        };
+        "custom/uptime" = {
+          exec = ./scripts/uptime.sh;
+          interval = "60";
+          tooltip = false;
+          format = "{}";
+        };
+        pulseaudio = {
+          format = "<span color='#cba6f7'>{icon}</span>{volume}%";
+          tooltip = false;
+          format-muted = "<span color='#f38ba8'> </span>Muted";
+          format-icons = {
+            default = [" " " " " "];
+          };
+          on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          scroll-step = 5;
+        };
+        temperature = {
+          tooltip = false;
+          format = "<span color='#ea76cb'>{icon}</span> {temperatureC}°C";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+        cpu = {
+          format = "<span color='#eba0ac'>{icon}</span>{usage}%";
+          format-icons = [
+            " "
+            "󰪞 "
+            "󰪟 "
+            "󰪠 "
+            "󰪡 "
+            "󰪢 "
+            "󰪣 "
+            "󰪤 "
+            "󰪥 "
+          ];
+          tooltip = false;
+        };
+        memory = {
+          format = "<span color='#fab387'>{icon}</span>{used}/{total}";
+          format-icons = [
+            " "
+            "󰪞 "
+            "󰪟 "
+            "󰪠 "
+            "󰪡 "
+            "󰪢 "
+            "󰪣 "
+            "󰪤 "
+            "󰪥 "
+          ];
+          tooltip = false;
+        };
+      };
+    };
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+        font-family: "JetBrainsMono NF";
+        font-size: 12.5px;
+        font-weight: bold;
+      }
+
+      window#waybar {
+        background-color: transparent;
+        color: #cdd6f4;
+      }
+
+      #workspaces button {
+        padding: 0 4px;
+        color: #cdd6f4;
+      }
+
+      #workspaces button.active {
+        color: #89dceb;
+      }
+
+      #workspaces button.urgent {
+        color: #f38ba8;
+      }
+
+      #workspaces button.persistent {
+        color: #f9e2af;
+      }
+
+      #workspaces button:hover {
+        background: #1e1e2e;
+      }
+
+      #custom-uptime,
+      #pulseaudio,
+      #clock,
+      #workspaces,
+      #disk,
+      #memory,
+      #tray,
+      #mode,
+      #temperature,
+      #cpu {
+        background: #1e1e2e;
+        color: #cdd6f4;
+        margin: 2px 3px;
+        padding: 0 10px;
+        border-radius: 5px;
+      }
+    '';
   };
 }
