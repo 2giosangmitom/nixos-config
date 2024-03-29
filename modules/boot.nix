4e9c8dcf570fa25c727a2f47a1fe7a8989b51326
Vo Quang Chien <voquangchien.dev@proton.me>
2024-03-29 15:54:37 +0700

    perf: disable core dumps

--- modules/boot.nix
+++ modules/boot.nix
@@ -25,7 +25,10 @@
       # A low value causes the kernel to prefer freeing up open files (page cache),
       # a high value causes the kernel to try to use swap space, and a value of 100 means IO cost is assumed to be equal.
       "vm.swappiness" = 100;
+      # Disable NMI watchdog
       "kernel.nmi_watchdog" = 0;
+      # Disable core dumps
+      "kernel.core_pattern" = "/dev/null";
     };
   };
 }
