7f32a8b64fc9237420924468df6b152da3e13cc6
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-04-07 10:22:52 +0700

    clean nixos config

--- modules/user.nix
+++
@@ -1,8 +0,0 @@
-{
-  users.users.chien = {
-    isNormalUser = true;
-    description = "Vo Quang Chien";
-    initialPassword = "123";
-    extraGroups = ["networkmanager" "wheel" "docker"];
-  };
-}
