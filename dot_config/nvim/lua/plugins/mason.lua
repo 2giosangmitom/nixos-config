fd885594ea0c02e6231e09bc2cc9cde34dd9eec7
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-02 17:04:04 +0700

    use AstroNvim

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,47 +1,43 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Mason plugins
+
+---@type LazySpec
 return {
-  "williamboman/mason.nvim",
-  cmd = "Mason",
-  build = ":MasonUpdate",
-  opts = {
-    ensure_installed = { "stylua", "lua-language-server" },
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "lua_ls",
+        -- add more arguments for adding more language servers
+      })
+    end,
   },
-  config = function(_, opts)
-    require("mason").setup(opts)
-
-    ---Install a package using Mason
-    ---@param package_name string
-    local function install_package(package_name)
-      local registry = require("mason-registry")
-      local optional = require("mason-core.optional")
-      local p = require("mason-core.package")
-      local source = p.Parse(package_name)
-
-      optional
-        .of_nilable(source)
-        :map(function(source_name)
-          local ok, pkg = pcall(registry.get_package, source_name)
-          if ok then return pkg end
-        end)
-        :if_present(function(pkg)
-          if not pkg:is_installed() then
-            vim.notify(("Installing %s"):format(pkg.name), vim.log.levels.INFO, { title = "mason.nvim" })
-            pkg:install()
-          end
-        end)
-    end
-
-    if type(opts.ensure_installed) == "table" then
-      ---@type table<string, boolean>
-      local added = {}
-      opts.ensure_installed = vim.tbl_filter(function(lang)
-        if added[lang] then return false end
-        added[lang] = true
-        return true
-      end, opts.ensure_installed)
-    end
-
-    for _, package_name in ipairs(opts.ensure_installed) do
-      install_package(package_name)
-    end
-  end,
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "prettier",
+        "stylua",
+        -- add more arguments for adding more null-ls sources
+      })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "python",
+        -- add more arguments for adding more debuggers
+      })
+    end,
+  },
 }
