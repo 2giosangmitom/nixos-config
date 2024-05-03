aefa6903876478b6d697306ec3131c121c7ab579
Vo Quang Chien <voquangchien.dev@proton.me>
2024-05-03 14:25:01 +0700

    use lazyvim

--- dot_config/nvim/lua/community.lua
+++
@@ -1,5 +0,0 @@
----@type LazySpec
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.json" },
-}
---
+++ dot_config/nvim/lua/config/lazy.lua
@@ -0,0 +1,45 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = true,
+    version = false,
+  },
+  dev = {
+    patterns = { "2giosangmitom", "LazyVim" },
+    path = "~/Workspace/neovim-plugins",
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
