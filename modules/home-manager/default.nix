3112dbaa77beb6e06a555f2ff2be56d3e309d786
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-17 13:10:45 +0700

    update config

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,19 @@
 {pkgs, ...}: {
-  imports = [./alacritty.nix ./sway.nix ./lazygit.nix ./shell.nix ./git.nix ./waybar.nix];
+  imports = [./alacritty.nix ./sway.nix ./lazygit.nix ./shell.nix ./git.nix ./waybar.nix ./theme.nix];
   programs.home-manager.enable = true;
+  programs.yazi = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      log = {
+        enabled = false;
+      };
+      manager = {
+        show_hidden = true;
+        sort_dir_first = true;
+      };
+    };
+  };
 
   home = {
     packages = with pkgs; [
