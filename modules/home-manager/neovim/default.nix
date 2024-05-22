6b8df159489eaf060c9609ee64b17c9e7202ea0f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-22 10:53:31 +0700

    update

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -1,9 +1,11 @@
-{lib,...}:{
+{pkgs,config,lib,...}:{
+  home.activation.installLazyVim = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${./config}/ ${config.xdg.configHome}/nvim/
+  '';
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     withRuby = false;
     withPython3 = false;
   };
-  home.file.".config/nvim".source = ./config;
 }
