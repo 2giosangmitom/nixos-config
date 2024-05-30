5eabda002ba1fa462e65e2dee5f242c3057e5b00
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-30 13:22:28 +0700

    improve coding style

--- modules/home-manager/scripts/random_bg.sh
+++ modules/home-manager/scripts/random_bg.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-if !(pgrep swww-daemon); then
+if ! (pgrep -x swww-daemon >/dev/null); then
 	swww clear-cache
-	swww-daemon --format xrgb &
+	if ! (swww-daemon --format xrgb &) then
+		echo "Failed to start swww-daemon" >&2
+		exit 1
+	fi
+	sleep 1
 fi
 
-swww img $(shuf -en1 /etc/nixos/backgrounds/*)
+BACKGROUND_IMAGE=$(shuf -en1 /etc/nixos/backgrounds/*)
+swww img "$BACKGROUND_IMAGE"
