5918d832079e67683d033d8490976d3b6d5d6356
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-14 21:06:40 +0700

    feat: implement nixos basic config

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,3 +1,37 @@
 {
+  pkgs,
+  config,
+  ...
+}: {
+  catppuccin.enable = true;
+  environment.systemPackages = with pkgs;
+    [
+      curl
+      gnumake
+      fd
+      tokei
+      du-dust
+      ripgrep
+      jq
+      zip
+      unzip
+      clang
+      procps
+      xdg-utils
+      bob-nvim
+      eza
+    ]
+    ++ pkgs.lib.optionals config.isGraphical [
+      brave
+      gh
+      pavucontrol
+    ];
+  security.polkit.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  systemd.services.systemd-journal-flush.enable = false;
+
   system.stateVersion = "24.05";
 }
