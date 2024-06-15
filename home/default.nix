164be8b88831a1d75e91f26bc80becd99b4d6b26
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-15 16:36:19 +0700

    feat: import programs

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,16 @@
-{config, ...}: {
+{
   imports = [
     ./window-manager/sway.nix
+    ./window-manager/hyprland.nix
+    ./programs/bash.nix
+    ./programs/starship.nix
+    ./programs/alacritty.nix
+    ./programs/fastfetch.nix
+    ./programs/git.nix
+    ./programs/lazygit.nix
+    ./programs/yazi.nix
+    ./programs/dunst.nix
+    ./programs/bottom.nix
   ];
   home = {
     stateVersion = "24.05";
