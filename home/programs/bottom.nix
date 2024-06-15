5b33fbd1c57353a6a6f835443f019e66786107ad
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-15 16:35:34 +0700

    feat: add bottom config

---
+++ home/programs/bottom.nix
@@ -0,0 +1,16 @@
+{
+  programs.bottom = {
+    enable = true;
+    catppuccin.enable = true;
+    settings = {
+      flags = {
+        dot_marker = true;
+        current_usage = true;
+        group_processes = true;
+        regex = true;
+        case_sensitive = false;
+        tree = true;
+      };
+    };
+  };
+}
