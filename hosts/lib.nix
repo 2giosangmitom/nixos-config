6dc3f00b5441371662af1a4511bd87bcf2e3a79e
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-16 10:50:07 +0700

    chore: simplify the config

--- hosts/lib.nix
+++ hosts/lib.nix
@@ -2,26 +2,24 @@
   inputs,
   overlays,
 }: rec {
-  hmCommonConfig = {username}: (
-    {
-      config,
-      pkgs,
-      ...
-    }: let
-      homeLib = import ../home/lib.nix {inherit inputs username;};
-    in {
-      config = {
-        nixpkgs = {inherit overlays;};
-        home-manager = {
-          inherit (homeLib) extraSpecialArgs;
-          sharedModules = homeLib.modules;
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          users.${username}.imports = [../home];
-        };
+  hmCommonConfig = {username}: {
+    config,
+    pkgs,
+    ...
+  }: let
+    homeLib = import ../home/lib.nix {inherit inputs username;};
+  in {
+    config = {
+      nixpkgs.overlays = overlays;
+      home-manager = {
+        inherit (homeLib) extraSpecialArgs;
+        sharedModules = homeLib.modules;
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.${username}.imports = [../home];
       };
-    }
-  );
+    };
+  };
 
   mkSystem = {
     host,
@@ -35,26 +33,20 @@
   in {
     nixosConfigurations.${host} = inputs.nixpkgs.lib.nixosSystem {
       inherit system;
-      specialArgs = {
-        inherit inputs;
-      };
+      specialArgs = {inherit inputs;};
       modules =
         [
           {
             options = {
-              dotfiles = {
-                username = mkOption {
-                  type = types.str;
-                  default = username;
-                  description = "The username for user";
-                };
-                window-manager = mkOption {
-                  type = types.nullOr (
-                    types.enum ["sway" "hyprland"]
-                  );
-                  default = "sway";
-                  description = "The window manager to use";
-                };
+              dotfiles.username = mkOption {
+                type = types.str;
+                default = username;
+                description = "The username for user";
+              };
+              dotfiles.window-manager = mkOption {
+                type = types.nullOr (types.enum ["sway" "hyprland"]);
+                default = "sway";
+                description = "The window manager to use";
               };
               isGraphical = mkOption {
                 type = types.bool;
@@ -62,16 +54,18 @@
                 description = "Whether the system is a graphical target";
               };
             };
-            config.dotfiles.window-manager = pkgs.lib.mkIf (!isGraphical) null;
-            config.networking.hostName = host;
+            config = {
+              dotfiles.window-manager = pkgs.lib.mkIf (!isGraphical) null;
+              networking.hostName = host;
+            };
           }
           ./${host}
           ../nixos/common
           ../nixos
           inputs.catppuccin.nixosModules.catppuccin
           inputs.home-manager.nixosModules.home-manager
+          (hmCommonConfig {inherit username;})
         ]
-        ++ [(hmCommonConfig {inherit username;})]
         ++ extraModules;
     };
   };
