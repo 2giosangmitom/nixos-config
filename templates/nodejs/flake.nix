2380456a575a331a6fea74acea5a53fddaa7eb4b
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-18 23:10:24 +0700

    feat: implement templates

---
+++ templates/nodejs/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NodeJS project flake template";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alejandra
+            nil
+            nodejs_20
+            corepack_20
+          ];
+        };
+      }
+    );
+}
