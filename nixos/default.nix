b3553e3624c1cb179786d3787e800d2773b797c2
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-29 13:28:33 +0700

    feat: add `minecraft-fhs` FHS environment

--- nixos/default.nix
+++ nixos/default.nix
@@ -17,6 +17,60 @@
       bob-nvim
       mesa
       libva
+
+      (pkgs.buildFHSUserEnv {
+        name = "minecraft-fhs";
+        targetPkgs = pkgs:
+          with pkgs; [
+            alsa-lib
+            atk
+            cairo
+            cups
+            dbus
+            expat
+            file
+            fontconfig
+            freetype
+            fuse
+            glib
+            gtk3
+            libGL
+            libnotify
+            libxml2
+            libxslt
+            netcat
+            nspr
+            nss
+            openjdk8
+            openssl.dev
+            pango
+            pkg-config
+            strace
+            udev
+            vulkan-loader
+            watch
+            wget
+            which
+            xorg.libX11
+            xorg.libxcb
+            xorg.libXcomposite
+            xorg.libXcursor
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXi
+            xorg.libXrandr
+            xorg.libXrender
+            xorg.libXScrnSaver
+            xorg.libxshmfence
+            xorg.libXtst
+            xorg.xcbutilkeysyms
+            zlib
+            fontconfig.lib
+          ];
+        profile = ''export FHS=1'';
+        runScript = "bash";
+      })
     ]
     ++ (with pkgs-unstable; [
       vscode.fhs
