475d15041d1fb3441f0139ba7682697efa495658
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-13 21:45:54 +0700

    remove minecraft-fhs

--- nixos/default.nix
+++ nixos/default.nix
@@ -19,66 +19,8 @@
       bob-nvim
       mesa
       libva
-
-      (pkgs.buildFHSUserEnv {
-        name = "minecraft-fhs";
-        targetPkgs = pkgs:
-          with pkgs; [
-            alsa-lib
-            atk
-            cairo
-            cups
-            dbus
-            expat
-            fontconfig
-            freetype
-            gdk-pixbuf
-            glib
-            pango
-            gtk3-x11
-            gtk2-x11
-            nspr
-            nss
-            stdenv.cc.cc
-            zlib
-            libuuid
-            jre8
-            cpio
-            file
-            giflib
-            gnome2.GConf
-            gnome2.gnome_vfs
-            gtk2
-            libjpeg
-            libGL
-            perl
-            which
-            xorg.libX11
-            xorg.libxcb
-            xorg.libXcomposite
-            xorg.libICE
-            xorg.libXinerama
-            xorg.xrandr
-            xorg.libXt
-            zip
-            zlib
-            xorg.libXcursor
-            xorg.libXdamage
-            xorg.libXext
-            xorg.libXfixes
-            xorg.libXi
-            xorg.libXrandr
-            xorg.libXrender
-            xorg.libXtst
-            xorg.libXScrnSaver
-            xorg.libXxf86vm
-          ];
-        profile = "export FHS=1";
-        runScript = "bash";
-      })
     ]
     ++ (with pkgs-unstable; [
-      vscode.fhs
       brave
       gh
       pavucontrol
@@ -99,8 +41,7 @@
       go
       wget
       nodejs_20
-      google-chrome
-      firefox
+      bleachbit
     ]);
 
   security.polkit.enable = true;
