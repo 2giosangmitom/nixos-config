b456e3fa6f6fa7ca53f99b91b9f655ec3870dcec
Vo Quang Chien <voquangchien.dev@proton.me>
2024-08-05 15:53:55 +0700

    feat: update zsh config to integrate with starship

--- nixos/common/shell.nix
+++ nixos/common/shell.nix
@@ -1,5 +1,25 @@
 { pkgs, ... }:
 {
   programs.zsh.enable = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+
+  programs.zsh.ohMyZsh = {
+    enable = true;
+    plugins = [ "cp" ];
+  };
+  programs.zsh.shellAliases = {
+    cat = "bat";
+
+    ls = "eza";
+    ll = "eza -l";
+    la = "eza -a";
+    lt = "eza -T";
+    lla = "eza -la";
+    llt = "eza -lT";
+
+    cp = "cp -i";
+    mv = "mv -i";
+    rm = "rm -i";
+  };
   users.defaultUserShell = pkgs.zsh;
 }
