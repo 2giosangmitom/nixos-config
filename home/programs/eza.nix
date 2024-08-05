89660ab5c9c88fb0c62a0cebae94e766ce8b9bb1
Vo Quang Chien <voquangchien.dev@proton.me>
2024-08-05 15:53:07 +0700

    feat: add eza

--- home/programs/eza.nix
+++ home/programs/eza.nix
@@ -1,1 +1,20 @@
-{}
+{ config, lib, ... }:
+let
+  cfg = config.dotfiles.eza;
+in
+{
+  options.dotfiles.eza = {
+    enable = lib.mkEnableOption "Eza";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.eza = {
+      enable = true;
+      icons = true;
+      extraOptions = [
+        "--group"
+        "--group-directories-first"
+      ];
+    };
+  };
+}
