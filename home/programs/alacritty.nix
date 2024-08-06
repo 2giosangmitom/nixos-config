e002962c6a6974b1097f55df7cf2efbbef746bf0
Vo Quang Chien <voquangchien.dev@proton.me>
2024-08-06 20:10:31 +0700

    feat: update alacritty colors

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -48,6 +48,22 @@ in
         window = {
           dynamic_padding = true;
         };
+        colors = {
+          primary = {
+            foreground = "#F8EDE3";
+            background = "#0C0C0C";
+          };
+          normal = {
+            black = "#1A2130";
+            red = "#C80036";
+            green = "#379777";
+            yellow = "#FFF078";
+            blue = "#6EACDA";
+            magenta = "#FF0080";
+            cyan = "#5DEBD7";
+            white = "#FCF8F3";
+          };
+        };
       };
     };
   };
