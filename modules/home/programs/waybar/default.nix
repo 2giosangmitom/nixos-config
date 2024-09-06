0d85b67832cb6c6ce060d7a3d64c7ef6e3f65547
Vo Quang Chien <voquangchien.dev@proton.me>
2024-09-06 22:52:36 +0700

    feat(waybar): update waybar theme

--- modules/home/programs/waybar/default.nix
+++ modules/home/programs/waybar/default.nix
@@ -15,14 +15,19 @@ in
           layer = "top";
           position = "top";
           mod = "dock";
+          spacing = 10;
           exclusive = true;
           passthrough = false;
           gtk-layer-shell = true;
-          height = 40;
+          height = 36;
+          reload_style_on_change = false;
+          margin-right = 10;
+          margin-left = 10;
           modules-left = [
             "sway/workspaces"
             "sway/mode"
           ];
+          modules-center = [ "clock" ];
           modules-right = [
             "pulseaudio"
             "network"
@@ -32,13 +37,84 @@ in
             "memory"
             "temperature"
             "cpu"
-            "clock"
             "power-profiles-daemon"
             "tray"
           ];
+          backlight = {
+            format = "<span color='#fab387'>󰃠</span> {percent}%";
+          };
+          cpu = {
+            interval = 10;
+            format = "<span color='#89b4fa'></span> {usage}%";
+            states = {
+              warning = 70;
+              critical = 90;
+            };
+          };
+          clock = {
+            format = "<span color='#a6e3a1'>󰥔</span> {:%H:%M - %a, %d %b}";
+            tooltip = false;
+          };
+          "sway/workspaces" = {
+            disable-scroll = true;
+            format = "{name}";
+            disable-click = true;
+            disable-markup = true;
+          };
+          pulseaudio = {
+            format = "<span color='#f4b8e4'>{icon}</span> {volume}%";
+            format-muted = "<span color='#e78284'></span> Muted";
+            format-icons = [
+              ""
+              ""
+              ""
+            ];
+            max-length = 10;
+            tooltip = false;
+          };
+          network = {
+            format = "{ifname}";
+            format-wifi = "<span color='#8caaee'>{icon}</span> {signalStrength}";
+            format-wifi-icons = [
+              "󰤟"
+              "󰤢"
+              "󰤥"
+              "󰤨"
+            ];
+            format-ethernet = "<span color='#8caaee'>󰈀</span> {ifname}";
+            format-disconnected = "<span color='#8caaee'>󰇧</span> Disconnected";
+            tooltip-format-wifi = "{essid} ({signalStrength}%)";
+          };
+          battery = {
+            states = {
+              warning = 20;
+              critical = 10;
+            };
+            format = "<span color='#ca9ee6'>{icon}</span> {capacity}%";
+            format-icons = [
+              "󰂎"
+              "󰁺"
+              "󰁻"
+              "󰁼"
+              "󰁽"
+              "󰁾"
+              "󰁿"
+              "󰂀"
+              "󰂁"
+              "󰂂"
+              "󰁹"
+            ];
+            tooltip-format = "Status: {timeTo}\nHealth: {health}%";
+          };
+          disk = {
+            interval = 30;
+            format = "<span color='#99d1db'>󰋊</span> {percentage_used}%";
+            path = "/";
+            unit = "GiB";
+            tooltip-format = "{used} used out of {total} on {path}";
+          };
         };
       };
-      style = ./style.css;
     };
   };
 }
