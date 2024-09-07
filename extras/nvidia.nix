70217f45f925cec254cc54212d01f6871c684914
Vo Quang Chien <voquangchien.dev@proton.me>
2024-09-07 22:12:37 +0700

    fix(nvidia): enable offload mode

--- extras/nvidia.nix
+++ extras/nvidia.nix
@@ -1,40 +1,55 @@
 { config, pkgs-unstable, ... }:
 {
-  boot.kernelParams = [ "amd_pstate=active" ];
-  boot.blacklistedKernelModules = [ "nouveau" ];
-  boot.extraModprobeConfig = ''
-    blacklist nouveau
-    options nouveau modeset=0
-  '';
-
   environment.systemPackages = with pkgs-unstable; [
     glxinfo
     egl-wayland
     vulkan-loader
   ];
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  environment.variables = {
+    "__GLX_VENDOR_LIBRARY_NAME" = "mesa";
+  };
+
+  services.udev.extraRules = ''
+    # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
+    ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
+    ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
+
+    # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
+    ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
+    ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
+
+    # Enable runtime PM for NVIDIA VGA/3D controller devices on adding device
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
+  '';
+
+  boot.extraModprobeConfig = ''
+    options nvidia "NVreg_DynamicPowerManagement=0x02"
+  '';
+
+  services.xserver.videoDrivers = [ "amdgpu" "nvidia" ];
+  boot.initrd.kernelModules = [ "amdgpu" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
 
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
-    extraPackages = with pkgs-unstable; [
-      vaapiVdpau
-      amdvlk
-      rocmPackages.clr
-      rocmPackages.clr.icd
-    ];
+    extraPackages = with pkgs-unstable; [ vaapiVdpau ];
   };
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.latest;
     modesetting.enable = true;
-    open = true;
+    open = false;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
     nvidiaSettings = true;
     prime = {
-      sync.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
       amdgpuBusId = "PCI:5:0:0";
       nvidiaBusId = "PCI:1:0:0";
     };
