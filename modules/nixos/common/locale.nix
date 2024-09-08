741579f98aceb5e89d52b1782864e177c0e53bee
Vo Quang Chien <voquangchien.dev@proton.me>
2024-09-08 10:18:49 +0700

    feat: add ibus engine

--- modules/nixos/common/locale.nix
+++ modules/nixos/common/locale.nix
@@ -1,7 +1,25 @@
+{ pkgs-unstable, ... }:
 {
   time = {
     timeZone = "Asia/Ho_Chi_Minh";
   };
 
   i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.variables = {
+    GTK_IM_MODULE = "ibus";
+    QT_IM_MODULE = "ibus";
+    XMODIFIERS = "@im=ibus";
+  };
+
+  environment.etc.environment.text = ''
+    GTK_IM_MODULE=ibus
+    QT_IM_MODULE=ibus
+    XMODIFIERS=@im=ibus
+  '';
+
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus.engines = with pkgs-unstable.ibus-engines; [ bamboo ];
+  };
 }
