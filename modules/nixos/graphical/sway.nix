69ce880d174b521d259b974844165dccbc8a3709
Vo Quang Chien <voquangchien.dev@proton.me>
2024-09-08 10:26:16 +0700

    chore(sway): update env variables

--- modules/nixos/graphical/sway.nix
+++ modules/nixos/graphical/sway.nix
@@ -27,8 +27,8 @@ in
       XDG_SESSION_DESKTOP = "sway";
       XDG_CURRENT_DESKTOP = "sway";
       GDK_BACKEND = "wayland,x11";
-      NIXOS_OZONE_WL = 1;
-      MOZ_ENABLE_WAYLAND = 1;
+      NIXOS_OZONE_WL = "1";
+      MOZ_ENABLE_WAYLAND = "1";
       QT_QPA_PLATFORM = "wayland";
       SDL_VIDEODRIVER = "wayland";
       _JAVA_AWT_WM_NONREPARENTING = 1;
