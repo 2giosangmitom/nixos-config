1f452e99e8f184cecac07359123d1a9693e64bec
Vo Quang Chien <voquangchien.dev@proton.me>
2024-09-09 11:33:13 +0700

    fix(nvidia): correct setup nvidia driver

--- extras/nvidia.nix
+++ extras/nvidia.nix
@@ -6,42 +6,7 @@
     vulkan-loader
   ];
 
-  services.udev.extraRules = ''
-    # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
-    ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-    ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
-
-    # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
-    ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
-    ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
-
-    # Enable runtime PM for NVIDIA VGA/3D controller devices on adding device
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
-  '';
-
-  boot.extraModprobeConfig = ''
-    options nvidia "NVreg_DynamicPowerManagement=0x02"
-    blacklist nouveau
-  '';
-
-  boot.kernelParams = [
-    "rd.driver.blacklist=nouveau"
-    "nvidia-drm.modeset=1"
-  ];
-
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [
-    "amdgpu"
-    "nvidia"
-  ];
-  boot.initrd.kernelModules = [
-    "amdgpu"
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
-  ];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.opengl = {
     enable = true;
