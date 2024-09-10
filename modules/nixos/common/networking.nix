5536bef6ae042af5d010b039f748b5a371809316
Vo Quang Chien <voquangchien.dev@proton.me>
2024-09-10 20:10:29 +0700

    chore(networking): enable firewall

--- modules/nixos/common/networking.nix
+++ modules/nixos/common/networking.nix
@@ -3,7 +3,7 @@
   networking.hostName = hostName;
   networking.proxy.noProxy = "127.0.0.1,localhost";
   networking.networkmanager.enable = true;
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
   networking.nameservers = [
     "8.8.8.8"
     "8.8.4.4"
