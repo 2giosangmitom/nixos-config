c69f298a43631fd52df5836709226f97b778d44d
Vo Quang Chien <voquangchien.dev@proton.me>
2024-10-03 16:32:03 +0700

    feat: enable bluetooh

--- hosts/nixos/nixos.nix
+++ hosts/nixos/nixos.nix
@@ -3,5 +3,9 @@
     graphical.hyprland.enable = true;
     # graphical.gnome.enable = true;
     services.hardware.openrgb.enable = true;
+    hardware.enableRedistributableFirmware = true;
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = false;
+    services.blueman.enable = true;
   };
 }
